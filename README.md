e34bf6ac78e2ed4f3bf23813539110c6d0e6b9c6
mveeprojects <markvallis1987@Gmail.com>
2025-05-31 23:13:39 +0100

    Added obfuscation code.

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 
 JavaJsonObfuscator is a tool designed to obfuscate/omit sensitive information from JSON files, making them less readable and more secure.
 
-Given a list of sensitive keys, the tool will check for any keys in the JSON that contain these values (case-insensitive), and then remove these keys from the JSON output.
+Given a list of sensitive keys, the tool will check for any keys in the JSON that contain these values (case-insensitive), and then remove or obfuscate these keys from the JSON output.
 
 ### Example input:
 
-When filtering by "email", "address", "creditcard", see `keywordsToObfuscate` list in Application.java.
+```java
+String jsonString = "{\"name\":\"Mark\",\"contactDetails\":{\"streetAddress\":\"somewhere\",\"email\":\"mark@mveeprojects.com\"},\"creditCardNumber\":\"1234-5678-9012-3456\"}";
+```
 
+As Json this look like::
 ```json
 {
   "name": "Mark",
@@ -20,8 +23,18 @@ When filtering by "email", "address", "creditcard", see `keywordsToObfuscate` li
 ```
 
 ### Example output:
+
+When filtering by "email", "address", "creditcard" and setting `runMode = "OBFUSCATE"`.
+
 ```text
-JSON after removal of sensitive fields: 
+JSON after processing of sensitive fields: 
+{"name":"Mark","contactDetails":{"streetAddress":"************","email":"************"},"creditCardNumber":"************"}
+```
+
+When filtering by "email", "address", "creditcard" and setting `runMode = "OMIT"`.
+
+```text
+JSON after processing of sensitive fields: 
 {"name":"Mark","contactDetails":{}}
 ```
 
@@ -30,3 +43,4 @@ JSON after removal of sensitive fields:
 * https://www.baeldung.com/java-jsonnode-get-keys
 * https://www.baeldung.com/jackson-object-mapper-tutorial
 * https://www.baeldung.com/java-jackson-remove-json-elements
+* https://www.baeldung.com/jackson-json-node-tree-model
--- src/main/java/org/mveeprojects/Application.java
+++ src/main/java/org/mveeprojects/Application.java
@@ -3,14 +3,17 @@ package org.mveeprojects;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.TextNode;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class Application {
+
+    static String runMode = "OBFUSCATE";
+//    static String runMode = "OMIT";
 
     static ObjectMapper om = new ObjectMapper();
 
@@ -22,18 +25,18 @@ public class Application {
 
     public static void main(String[] args) throws IOException {
         String jsonString = "{\"name\":\"Mark\",\"contactDetails\":{\"streetAddress\":\"somewhere\",\"email\":\"mark@mveeprojects.com\"},\"creditCardNumber\":\"1234-5678-9012-3456\"}";
-        System.out.println("JSON after removal of sensitive fields: ");
+        System.out.println("JSON after processing of sensitive fields: ");
         System.out.println(getAllKeysInJsonUsingJsonNodeFieldNames(jsonString, om));
-
-//        -- Output: --
-//        JSON after removal of sensitive fields:
-//        {"name":"Mark","contactDetails":{}}
     }
 
-    public static JsonNode getAllKeysInJsonUsingJsonNodeFieldNames(String json, ObjectMapper mapper) throws JsonProcessingException {
-        List<String> keys = new ArrayList<>();
-        JsonNode jsonNode = mapper.readTree(json);
-        return removeFieldsFromJson(jsonNode);
+    public static JsonNode getAllKeysInJsonUsingJsonNodeFieldNames(String jsonString, ObjectMapper mapper) throws JsonProcessingException {
+        JsonNode rootNode = mapper.readTree(jsonString);
+
+        if (runMode.equals("OMIT")) {
+            return removeFieldsFromJson(rootNode);
+        } else {
+            return obfuscateFieldsInJson(rootNode);
+        }
     }
 
     static JsonNode removeFieldsFromJson(JsonNode jsonNode) {
@@ -52,8 +55,26 @@ public class Application {
                 });
             }
         }
+        return jsonNode;
+    }
 
+    static JsonNode obfuscateFieldsInJson(JsonNode jsonNode) {
+        Iterator<Map.Entry<String, JsonNode>> fields = jsonNode.fields();
+        while (fields.hasNext()) {
+
+            Map.Entry<String, JsonNode> field = fields.next();
+
+            if (field.getValue().isObject() || field.getValue().isArray()) {
+                obfuscateFieldsInJson(field.getValue());
+            } else {
+                keywordsToObfuscate.forEach(keywordToObfuscate -> {
+                    if (field.getKey().toLowerCase().contains(keywordToObfuscate.toLowerCase())) {
+                        JsonNode obfuscatedNode = new TextNode("************");
+                        field.setValue(obfuscatedNode);
+                    }
+                });
+            }
+        }
         return jsonNode;
     }
 }
-
